357ebb3b63e35158a399a25ad27034b7561d17fc
James Higgins <james.isaac.higgins@gmail.com>
2021-06-28 14:48:48 -0300

    Remove Image and implementations of spAtlasPage create/dispose texture

--- spine/src/impl.rs
+++ spine/src/impl.rs
@@ -5,7 +5,6 @@ use std::{
     os::raw::{c_char, c_int, c_void},
 };
 
-use image::{DynamicImage, GenericImageView};
 use spine_sys::spAtlasPage;
 
 use super::{error::Error, result::Result};
@@ -39,31 +38,6 @@ pub extern "C" fn _spUtil_readFile(path: *const c_char, length: *mut c_int) -> *
     unsafe {
         *length = text.to_bytes().len() as c_int;
         text.into_raw()
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn _spAtlasPage_createTexture(page: *mut spAtlasPage, path: *const c_char) {
-    #[inline]
-    fn read_texture_file(path: *const c_char) -> Result<DynamicImage> {
-        let path = to_str(path)?;
-        image::open(path).map_err(Error::invalid_data)
-    }
-
-    let texture = read_texture_file(path).unwrap();
-    let (width, height) = texture.dimensions();
-
-    unsafe {
-        (*page).width = width as c_int;
-        (*page).height = height as c_int;
-        (*page).rendererObject = Box::into_raw(Box::new(texture)) as *mut c_void;
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn _spAtlasPage_disposeTexture(page: *mut spAtlasPage) {
-    unsafe {
-        Box::from_raw((*page).rendererObject as *mut DynamicImage);
     }
 }
 
